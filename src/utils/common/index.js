e2d4150551a7407c25d5ee3e96389b585c7b26ab
Tushar Agarwal <tushar7436@gmail.com>
2025-09-09 02:35:23 +0530

    added models for many to many assoc between roles and users

--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -1,6 +1,6 @@
 module.exports= {
     ErrorResponse: require('./error-response'),
     SuccessResponse: require('./success-response'),
-    ENUMS: require('./enum'),
+    ENUMS: require('./enums'),
     Auth: require('./auth')
 }
